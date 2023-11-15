4d684a3b0199f6e7732a7be08fa31f131bbe7bba
Alan Greer <alan.greer@diamond.ac.uk>
2023-11-15 15:08:58 +0000

    Implement ParameterTree for odin data adapters
    Update frame_processor_adapter to utilise correctly managed
    messages and implement a ParameterTree.
    Created new controller classes that update a parameter tree with
    config/status
    Created adapters to interact with the controller classes.
    Swap rank and status/config in API structure
      e.g. fp/status/0/... -> fp/0/status/...
    Expand endpoints list param into scalar ip and port
    
    Co-authored-by: Gary Yendell <gary.yendell@diamond.ac.uk>

--- python/src/odin_data/control/ipc_tornado_client.py
+++ python/src/odin_data/control/ipc_tornado_client.py
@@ -1,12 +1,15 @@
 import logging
 from threading import RLock
 from time import sleep
+from time import sleep
 
 from odin_data.control.ipc_message import IpcMessage, IpcMessageException
 from odin_data.control.ipc_tornado_channel import IpcTornadoChannel
 
 
 class IpcTornadoClient(object):
+    """Client class that wraps an IpcTornadoClient class with additional
+    message handling and management.
     """Client class that wraps an IpcTornadoClient class with additional
     message handling and management.
 
@@ -32,11 +35,23 @@ class IpcTornadoClient(object):
         :param ip_address: IP address for the ZeroMQ socket endpoint
         :param port: UPort number for the ZeroMQ socket endpoint
         """
+        """Initalise the IpcTornadoClient object.
+
+        This creates a DEALER ZeroMQ socket for communication with the following
+        applications:
+        - FrameReceiver
+        - FrameProcessor
+        - MetaWriter
+
+        :param ip_address: IP address for the ZeroMQ socket endpoint
+        :param port: UPort number for the ZeroMQ socket endpoint
+        """
         self.logger = logging.getLogger(self.__class__.__name__)
 
         self._ip_address = ip_address
         self._port = port
 
+        self._parameters = {self.IPC_VAL_STATUS: {self.CLIENT_CONNECTED: False}}
         self._parameters = {self.IPC_VAL_STATUS: {self.CLIENT_CONNECTED: False}}
         self.ctrl_endpoint = self.ENDPOINT_TEMPLATE.format(IP=ip_address, PORT=port)
         self.logger.debug("Connecting to client at %s", self.ctrl_endpoint)
@@ -45,6 +60,10 @@ class IpcTornadoClient(object):
         self.ctrl_channel.connect(self.ctrl_endpoint)
         self.ctrl_channel.register_callback(self._callback)
         self.message_id = 0
+
+        # Dictionary of outstanding config messages
+        self._active_msgs = {}
+        self._rejected_configs = {}
 
         # Dictionary of outstanding config messages
         self._active_msgs = {}
@@ -62,7 +81,29 @@ class IpcTornadoClient(object):
 
         :return: the _parameters dict
         """
+        """Read out the stored parameters from this client.
+
+        The parameters dictionary contains a snapshot of all responses to
+        messages sent out by this client.  The latest resposnes are
+        stored in the _parameters dict and are returned by this method.
+
+        :return: the _parameters dict
+        """
         return self._parameters
+
+    def read_rejected_configs(self):
+        """Return the current dict of rejected configuration messages.
+
+        :return: a dict of rejected configuration
+        """
+        return self._rejected_configs
+
+    def clear_rejected_configs(self):
+        """Clear any rejected configuration messages.
+        """
+        self.logger.debug("Clearing rejected configurations")
+        with self._lock:
+            self._rejected_configs.clear()
 
     def read_rejected_configs(self):
         """Return the current dict of rejected configuration messages.
@@ -96,8 +137,10 @@ class IpcTornadoClient(object):
         if msg['event'] == IpcTornadoChannel.CONNECTED:
             self.logger.debug("  Connected...")
             self._parameters[self.IPC_VAL_STATUS][self.CLIENT_CONNECTED] = True
+            self._parameters[self.IPC_VAL_STATUS][self.CLIENT_CONNECTED] = True
         if msg['event'] == IpcTornadoChannel.DISCONNECTED:
             self.logger.debug("  Disconnected...")
+            self._parameters[self.IPC_VAL_STATUS][self.CLIENT_CONNECTED] = False
             self._parameters[self.IPC_VAL_STATUS][self.CLIENT_CONNECTED] = False
 
     def _callback(self, msg):
@@ -118,12 +161,32 @@ class IpcTornadoClient(object):
 
         :param msg: Incoming message from the main ZeroMQ socket
         """
+        """Handes incoming messages from the main ZeroMQ socket.
+
+        The handled message types are:
+        - Reply to 'request_version'
+        - Reply to 'request_configuration'
+        - Reply to 'status'
+        - Reply to 'configure'
+
+        Replies to request_version, request_configuration and status
+        are stored in the _parameters according to the message type.
+        Replies to 'configure' result in the corresponding message
+        being removed from the _active_msgs dict.  They are then
+        checked for rejection and moved into the _rejected_configs
+        dict if they were rejected.
+
+        :param msg: Incoming message from the main ZeroMQ socket
+        """
         # Handle the multi-part message
         reply = IpcMessage(from_str=msg[0])
         if self.IPC_VAL_REQ_VERS in reply.get_msg_val():
+        if self.IPC_VAL_REQ_VERS in reply.get_msg_val():
             self._update_versions(reply.attrs)
         elif self.IPC_VAL_REQ_CFG in reply.get_msg_val():
+        elif self.IPC_VAL_REQ_CFG in reply.get_msg_val():
             self._update_configuration(reply.attrs)
+        elif self.IPC_VAL_STATUS in reply.get_msg_val():
         elif self.IPC_VAL_STATUS in reply.get_msg_val():
             self._update_status(reply.attrs)
         with self._lock:
@@ -131,8 +194,18 @@ class IpcTornadoClient(object):
             if reply.get_msg_type() == "nack" and reply.get_msg_val() == "configure":
                 with self._lock:
                     self._rejected_configs[reply.get_msg_id()] = reply
+        with self._lock:
+            self._active_msgs.pop(reply.get_msg_id())
+            if reply.get_msg_type() == "nack" and reply.get_msg_val() == "configure":
+                with self._lock:
+                    self._rejected_configs[reply.get_msg_id()] = reply
 
     def _update_versions(self, version_msg):
+        """Store the response to a request_version message in the
+        _parameters dict.
+
+        :param version_msg: Incoming version message response
+        """
         """Store the response to a request_version message in the
         _parameters dict.
 
@@ -147,10 +220,19 @@ class IpcTornadoClient(object):
 
         :param config_msg: Incoming configuration message response
         """
+        """Store the response to a request_configuration message in the
+        _parameters dict.
+
+        :param config_msg: Incoming configuration message response
+        """
         params = config_msg['params']
         self._parameters['config'] = params
 
     def _update_status(self, status_msg):
+        """Store the response to a status message in the _parameters dict.
+
+        :param status_msg: Incoming status message response
+        """
         """Store the response to a status message in the _parameters dict.
 
         :param status_msg: Incoming status message response
@@ -164,10 +246,23 @@ class IpcTornadoClient(object):
             for msg in self._rejected_configs:
                 self._parameters[self.IPC_VAL_STATUS]['error'].append(self._rejected_configs[msg].get_params()['error'])
 
+        self._parameters[self.IPC_VAL_STATUS] = params
+        if 'error' not in self._parameters[self.IPC_VAL_STATUS]:
+            self._parameters[self.IPC_VAL_STATUS]['error'] = []
+        with self._lock:
+            for msg in self._rejected_configs:
+                self._parameters[self.IPC_VAL_STATUS]['error'].append(self._rejected_configs[msg].get_params()['error'])
+
         # If we have received a status response then we must be connected
+        self._parameters[self.IPC_VAL_STATUS][self.CLIENT_CONNECTED] = True
         self._parameters[self.IPC_VAL_STATUS][self.CLIENT_CONNECTED] = True
 
     def connected(self):
+        """Returns the connected state of this client.
+
+        :return: The connected state of the client
+        """
+        return self._parameters[self.IPC_VAL_STATUS][self.CLIENT_CONNECTED]
         """Returns the connected state of this client.
 
         :return: The connected state of the client
@@ -182,12 +277,22 @@ class IpcTornadoClient(object):
 
         :param msg: The message to send
         """
+        """Injects the next message ID into the message object.
+        Adds the message to the list of active messages (indexed by
+        unique ID).  Sends the encoded ASCII message through the
+        ZeroMQ socket.
+
+        :param msg: The message to send
+        """
         msg.set_msg_id(self.message_id)
         self.message_id = (self.message_id + 1) % self.MESSAGE_ID_MAX
         self.logger.debug("Sending control message [%s]:\n%s", self.ctrl_endpoint, msg.encode())
         with self._lock:
             self._active_msgs[msg.get_msg_id()] = msg
+            self._active_msgs[msg.get_msg_id()] = msg
             self.ctrl_channel.send(msg.encode())
+
+        return msg
 
         return msg
 
@@ -204,10 +309,23 @@ class IpcTornadoClient(object):
 
         :param value: Value of the IpcMessage object
         """
+        """Creates an IpcMessage object with the specified value
+        and calls the _send_message method with the message object.
+
+        :param value: Value of the IpcMessage object
+        """
         msg = IpcMessage("cmd", value)
+        return self._send_message(msg)
         return self._send_message(msg)
 
     def send_configuration(self, content, target=None, valid_error=None):
+        """Creates a specific configure IpcMessage object.  Sets the
+        parameters of the configure message object to content and sends
+        the message
+
+        :param content: contents of message to send
+        :return: The message object that has been created and sent
+        """
         """Creates a specific configure IpcMessage object.  Sets the
         parameters of the configure message object to content and sends
         the message
